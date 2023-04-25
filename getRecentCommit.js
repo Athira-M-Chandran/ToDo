646076d6447c14aa21ad400c09f94fa9f633a401
Athira-M-Chandran <athiramchandran90@gmail.com>
2023-04-25 11:59:51 +0530

    n21

--- getRecentCommit.js
+++ getRecentCommit.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
-async function getLastCommit(owner, repo, token, branch='newBranch') {
-  const endpoint = `https://api.github.com/repos/${owner}/${repo}/commits/${branch}`;
+async function getLastCommit(owner, repo, token) {
+  const endpoint = `https://api.github.com/repos/${owner}/${repo}/commits/branches`;
   const headers = {
     Authorization: `Bearer ${token}`,
   };
@@ -15,15 +15,16 @@ async function getLastCommit(owner, repo, token, branch='newBranch') {
     message: recentCommit.commit.message,
     author: recentCommit.commit.author.name,
     timestamp: recentCommit.commit.author.date,
-    url: recentCommit.commit.url
+    url: recentCommit.commit.url,
+    branch: recentCommit.commit.branch
     
   };
   
   return commitInfo;
 }
 
-async function getCommitFiles(owner, repo, commitSha, token, branch='newBranch') {
-  const endpoint = `https://api.github.com/repos/${owner}/${repo}/commits/${commitSha}/${branch}`;
+async function getCommitFiles(owner, repo, commitSha, token) {
+  const endpoint = `https://api.github.com/repos/${owner}/${repo}/commits/${commitSha}`;
   const headers = {
     Authorization: `Bearer ${token}`,
   };
@@ -42,12 +43,12 @@ async function main() {
   try {
     const owner = 'Athira-M-Chandran';
     const repo = 'ToDo';
-    const token = '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';
+    const token = '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';
 
-    const recentCommit = await getLastCommit(owner, repo, token,'newBranch');
+    const recentCommit = await getLastCommit(owner, repo, token);
     console.log('recentCommit',recentCommit);
 
-    const commitFiles = await getCommitFiles(owner, repo, recentCommit.url.split('/').pop(), token, 'newBranch');
+    const commitFiles = await getCommitFiles(owner, repo, recentCommit.url.split('/').pop(), token);
     console.log('commit files:', commitFiles);
 
     const commitInfo = {
